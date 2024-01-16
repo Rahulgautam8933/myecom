6ff66b934926cc8ff089a37d08cd32f8c73e0b2c
rahul <rahulgautam8933@gmail.com>
2024-01-16 17:27:21 +0530

    profile

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,6 @@ function App() {
           <Route path="/admin" element={<Dashboard />}></Route>
           <Route path="/profile" element={<Profile />}></Route>
         </Routes>
-
         <Footer />
       </Router>
     </>
